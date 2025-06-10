5a8818d61b3d2f2ae39d589ba7f8a44cd3db898f
MeridjanShoti <meridjanshoti@gmail.com>
2025-06-10 17:24:19 +0200

    abbellimento contatti

--- src/contacts/Contacts.jsx
+++ src/contacts/Contacts.jsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 import { Image } from "react-bootstrap";
 import image from "../assets/img/meridjan.png";
 import { Envelope, Github, Linkedin } from "react-bootstrap-icons";
@@ -7,11 +7,11 @@ function Contacts() {
   return (
     <>
       <h1 className="text-center metal-mania-regular my-3">Meridjan Shoti</h1>
-      <Container xs={1} md={2} className="d-flex justify-content-center gap-5">
+      <Container className="d-flex flex-column flex-md-row justify-content-center gap-5">
         <div>
           <Image src={image} fluid className="rounded-5 border border-primary border-3" />
         </div>
-        <div className="d-flex flex-column justify-content-center fs-3">
+        <div className="d-flex flex-column justify-content-center fs-5">
           <p>
             <Github /> <a href="https://github.com/MeridjanShoti">https://github.com/MeridjanShoti</a>
           </p>
